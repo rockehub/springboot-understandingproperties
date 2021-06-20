c8e70fde404a5761b732aa2998a9d8a624ae06c6
Daniel Moreira <danielmoreira@pop-os.localdomain>
2021-06-20 19:44:09 -0300

    entendendo propriedades

--- src/main/java/study/in/understandproperties/controller/RestEnpoints.java
+++ src/main/java/study/in/understandproperties/controller/RestEnpoints.java
@@ -1,7 +1,11 @@
 package study.in.understandproperties.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
 
 @RestController
 public class RestEnpoints {
@@ -10,13 +14,25 @@ public class RestEnpoints {
     private String cName;
     @Value("${default.course.chapterCount}")
     private int cChapterCount;
+    @Autowired
+    private CourseConfiguration CourseConfiguration;
 
-
-    @RequestMapping("/defaultCourse")
+    @RequestMapping("/defaultcourse")
     public Course getDefaultCourse(@RequestParam(value = "name", defaultValue = "Spring Boot", required = false) String name,
-                              @RequestParam(value = "chapterCount", defaultValue = "2", required = false) int chapterCount
+                                   @RequestParam(value = "chapterCount", defaultValue = "2", required = false) int chapterCount
     ) {
         return new Course(cName, cChapterCount);
+
+    }
+
+    @RequestMapping("/gethierarchical")
+    public HashMap<String, Object> getConfigAnnotationProperties() {
+        HashMap<String, Object> map= new HashMap<String, Object>();
+        map.put("name", CourseConfiguration.getName());
+        map.put("chapterCount", CourseConfiguration.getChapterCount());
+        map.put("rating", CourseConfiguration.getRating());
+        map.put("author", CourseConfiguration.getAuthor());
+        return map;
 
     }
 
